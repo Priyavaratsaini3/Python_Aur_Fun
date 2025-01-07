f47b3ff00f9e0bb57969b7e6cb5d19298ce1bc4e
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-07 11:00:07 +0530

    ifelse condition

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,10 @@
+weather = "sunny"
+
+if weather == "sunny":
+    activity = "Go for a walk"
+elif weather == "rainy":
+    activity =  "Read a book"
+elif weather == "snowy":
+    activity = "Build a snowman"
+
+print(activity)
