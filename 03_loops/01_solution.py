8c367b453a17c61de7fd930d509163a86c9f8603
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-07 11:01:33 +0530

    loops

---
+++ 03_loops/01_solution.py
@@ -0,0 +1,6 @@
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+positve_number_count = 0
+for num in numbers:
+    if num > 0:
+        positve_number_count += 1
+print("Final count of positive number is:", positve_number_count)
