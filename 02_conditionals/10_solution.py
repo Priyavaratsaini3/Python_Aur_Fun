78e3b1541f302c5380ab6fe44314a92d7421bdb6
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-07 17:32:17 +0530

    solved

---
+++ 02_conditionals/10_solution.py
@@ -0,0 +1,12 @@
+dog_age  = int(input("Enter the dog age: "))
+cat_age =  int(input("Enter the cat age: "))
+
+if dog_age < 2:
+    print("serve the puppy food")
+else:
+    print("serve the dog food")
+
+if cat_age < 2:
+    print("serve the kitty food")
+else:
+    print("serve the cat food")
