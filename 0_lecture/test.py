446e835da6ffa94ba0daca48b6a61a63f8ecb98b
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-08 10:48:41 +0530

    add two string characters

--- 0_lecture/test.py
+++ 0_lecture/test.py
@@ -1,2 +1,21 @@
-import numpy as np
-print(np.array([1,2,3],[4,5,6]))
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        word1 = "abc"
+        word2 = "pqr"
+
+        result = ""
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                result += word1[i]
+            if i < len(word2):
+                result += word2[i]
+
+        word1 = word1.replace('c', '')
+        word2 += 's'
+        result1 = ""
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                result1 += word1[i]
+            if i < len(word2):
+                result1 += word2[i]
+        return result
