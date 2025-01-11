f1b1c19fafabf68b8720e833d905112cbf55add9
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-11 16:04:40 +0530

    chnages

--- 03_loops/loops_practice.py
+++ 03_loops/loops_practice.py
@@ -51,3 +51,58 @@ thislist = ["apple", "banana", "cherry"]
 thistuple = ("kiwi", "orange")
 thislist.extend(thistuple)
 print(thislist)
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+newlist = [x for x in fruits if x != "apple"]
+print(newlist)
+
+newlist = [x for x in fruits]
+print(newlist)
+
+newlist = [x.upper() for x in fruits]
+print(newlist)
+
+newlist = ['hello' for x in fruits]
+print(newlist)
+
+newlist = [x if x != "banana" else "orange" for x in fruits]
+print(newlist)
