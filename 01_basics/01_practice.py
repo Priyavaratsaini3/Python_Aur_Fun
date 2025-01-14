bdd00a987ad39a40c80069611c2e5144eaa33237
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-14 21:43:40 +0530

    practice of data type and variable

---
+++ 01_basics/01_practice.py
@@ -0,0 +1,19 @@
+# Write a Python program that prints "Hello, World!" to the console.
+
+print("Hello, Python!")
+
+# Create a program that asks the user for their name and age and prints a greeting like:
+# "Hello [Name], you are [Age] years old!"
+
+name = input()
+age = input()
+print(f"Hello {name}, you are {age} years old!")
+
+# Write a Python program that does the following:
+# Assign the value 5 to a variable x.
+# Assign the value 10 to a variable y.
+# Print the sum of x and y.
+
+x = 5
+y = 10
+print(x + y)
