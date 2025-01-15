147f62d797bbc4c49041eecad18d02597449e3a4
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-15 21:10:51 +0530

    find the area and circum of a circle using math.pi

---
+++ 01_basics/circle.py
@@ -0,0 +1,8 @@
+import math
+radius = int(input("Enter the Radius: "))
+area = math.pi * radius **2
+
+print("The area of a circle is : ", area)
+
+circum = 2 * math.pi * radius
+print("The circumference of a circle is: ", circum)
