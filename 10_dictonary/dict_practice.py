35a5440caa5968615af50495eb3e63508f851358
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-15 21:13:59 +0530

    dictionary practice

---
+++ 10_dictonary/dict_practice.py
@@ -0,0 +1,21 @@
+thisdict = {
+    "Barnd" : "Ford",
+    "Model" : "Mustang",
+    "Year" : "1964",
+    "Year" : "2020"
+}
+print(thisdict)
+print(len(thisdict))
+print(thisdict["Barnd"])
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict)
+print(type(thisdict))
+
+thisdict = dict(name = "john", age = 36, country = "Norway")
+print(thisdict)
