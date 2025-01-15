efb55b684be35dcbd9cb496de6dd8507f7e85a0a
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-15 21:11:27 +0530

    number check is positive or negative

---
+++ 01_basics/number_check.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number:"))
+if num > 0:
+    print("The number is positive: ", num)
+elif num < 0:
+    print("The number is negative:", num)
+else:
+    print("The number is Zero:", num)
