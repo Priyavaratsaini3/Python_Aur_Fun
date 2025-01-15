873634d946ba5c7c401c5999392c4f9e3e622b9b
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-15 10:52:46 +0530

    array indexing practice

--- numpy/01_practice.py
+++ numpy/01_practice.py
@@ -48,3 +48,13 @@ print(arr[2] + arr[3])
 #2d array indexing
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print('2nd element on 1st row:', arr[0, 1])
+
+print('5th element on 2nd row:',arr[1,4])
+
+#3d array indexing
+arr = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print('3rd element of 2nd array of the 1st array :',arr[0,1,2])
+
+# negative indexing
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('Last element from 2nd dim: ', arr[1,-1])
