81da2cff006d4452c4d83bb7f3353612107425ef
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-15 21:10:15 +0530

    practice some questions

---
+++ 01_basics/03_practice.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the number1 :"))
+num2 = int(input("Enter the number2 :"))
+
+sum = num1 + num2
+print("The sum of two number:",sum)
+
+difference = num1 - num2
+print("The difference of two number:", difference)
+
+product = num1 * num2
+print("The product of two number :", product)
+
+quotient = num1 / num2
+print("Quotient of two number: ", quotient)
