fe9483963a9d736c9c25cc8edb2ae79d34758754
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-15 21:13:42 +0530

    set syntax practice

--- 09_sets/set_practice.py
+++ 09_sets/set_practice.py
@@ -77,3 +77,42 @@ set2 = {"google", "microsoft", "apple"}
 set1.intersection_update(set2)
 
 print(set1)
+
+set1 = {"apple", 1, "banana", "cherry",0}
+set2 = {False, "google", 1, "apple", 2, True}
+set3 = set1.intersection(set2)
+print(set3)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1.difference(set2)
+set3 = set1 - set2
+print(set3)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(set1)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
+print(set3)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 ^ set2
+print(set3)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.symmetric_difference_update(set2)
+
+print(set1)
