43882c67848887f8d12da18a2123cfa6e6dde00c
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-17 11:27:34 +0530

    create a storyline

---
+++ projects/Text-Based Adventure Game/text_based_game.py
@@ -0,0 +1,13 @@
+import time 
+
+print("\n You wake up in a dark forest.")
+time.sleep(1.5)
+
+print("\n Tall, twisted trees surround you, their braches reaching like claws ")
+time.sleep(1.5)
+
+print("\n In the distance, you see the ruins of an ancient kingdom...")
+time.sleep(2)
+
+print("\n A mysterious voice whispers:'Find the Crystal of Light , or be trapped forever in this dark forest!'")
+time.sleep(2)
