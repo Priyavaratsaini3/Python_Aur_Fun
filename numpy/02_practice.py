565fdad883f0b6e5de587ac95f2895cfe2cf81d4
priyavarat saini <priyavartsaini3@gmail.com>
2025-01-29 21:07:57 +0530

    data type of numpy array

--- numpy/02_practice.py
+++ numpy/02_practice.py
@@ -19,3 +19,35 @@ arr = np.array([1,2,3,4,5,6,7])
 print(arr[::2])
 
 # 2D array Slicing
+
+arr = np.array(['apple', 'banana', 'cherry'])
+print(arr.dtype)
+
+arr = np.array([2,3,4,55])
+print(arr.dtype)
+
+arr = np.array([1,2,3,4,5,6,7], dtype='S')
+print(arr)
+print(arr.dtype)
+
+arr = np.array([1,2,3,4,5,6,7], dtype='i4')
+print(arr)
+print(arr.dtype)
+
+# arr = np.array(['a', '2', '3'], dtype='i')
+# print(arr)
+
+arr = np.array([1.1, 2.1, 3.1])
+newarr = arr.astype('i')
+print(newarr)
+print(newarr.dtype)
+
+arr = np.array([1,0,3])
+newarr = arr.astype(bool)
+print(newarr)
+print(newarr.dtype)
+
+arr = np.array([1.11,2.34,3.55,4.5,5.5])
+newarr = arr.astype(int)
+print(newarr)
+print(newarr.dtype)
