3cfa1410745751fee22ff356aff9f25bd5988e49
priyavarat saini <priyavartsaini3@gmail.com>
2025-03-12 08:26:48 +0530

    over write the text file

--- 0_lecture/file_input_output.py
+++ 0_lecture/file_input_output.py
@@ -1,4 +1,9 @@
-f = open(r"0_lecture\sample.txt", "r")
-data = f.readline()
-print(data)
+f = open(r"0_lecture\sample.txt", "w")
+# data = f.readline()
+# print(data)
+# f.close()
+
+f.write("I want to learn python")
+
 f.close()
+
