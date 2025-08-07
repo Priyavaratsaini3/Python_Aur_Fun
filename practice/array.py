0927986a025966253aa8cb56a3758d4028a35b1b
priyavaratsaini3 <priyavartsaini3@gmail.com>
2025-08-07 06:38:33 +0530

    loop pattern and arrays

---
+++ practice/array.py
@@ -0,0 +1,27 @@
+# import array as arr
+import numpy as np
+# newarr = arr.array('i', [1, 2, 3, 4, 5])
+# newarr.append(11)
+# print(newarr)
+
+# original_array = arr.array('i', [1, 3, 5, 7, 1, 9, 3])
+# org = original_array[::-1]
+# print(org)
+
+# original_array = arr.array('i', [1, 3, 5, 7, 1, 9, 3])
+# org = original_array.itemsize
+# print(org)
+
+# ogr = np.array([1, 3, 4, 5, 6, 2, 2, 3, 4, 8, 2, 3, 4, 5, 5], dtype=np.int16)
+# ogrs = ogr.itemsize
+# print(ogrs)
+# print(ogr)
+
+# for i in range(10):
+#     print(i, end=" ")
+
+number = int(input("Enter the no of rows: "))
+for i in range(1,number+1):
+    for j in range (i):
+        print("*", end=" ")
+    print( )
