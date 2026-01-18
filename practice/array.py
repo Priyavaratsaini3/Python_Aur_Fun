0bef0908770b0870698bd2ce3383e75973023313
priyavaratsaini3 <priyavartsaini3@gmail.com>
2026-01-18 21:48:23 +0530

    searching the elements maximum and minimum in the array

--- practice/array.py
+++ practice/array.py
@@ -1,4 +1,4 @@
-# import array as arr
+import array as arr
 import numpy as np
 # newarr = arr.array('i', [1, 2, 3, 4, 5])
 # newarr.append(11)
@@ -20,8 +20,20 @@ import numpy as np
 # for i in range(10):
 #     print(i, end=" ")
 
-number = int(input("Enter the no of rows: "))
-for i in range(1,number+1):
-    for j in range (i):
-        print("*", end=" ")
-    print( )
+# number = int(input("Enter the no of rows: "))
+# for i in range(1,number+1):
+#     for j in range (i):
+#         print("*", end=" ")
+#     print( )
+
+# def reverseArray(array):
+#     start, end=0, len(array) - 1
+#     while start < end:
+#         array[start], array[end] = array[end], array[start]
+#         start += 1
+#         end -= 1
+
+# array = [10, 20, 30, 40, 50, 60]
+# reverseArray(array)
+# print(array)
+
